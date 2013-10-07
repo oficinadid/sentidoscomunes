e769fa9465ae72c9be3cd23209b8d71a0051ef41
david s <messthetic@gmail.com>
2013-10-07 17:42:03 -0300

    llamamos a main.css en header

--- wp-content/themes/sentidos/header.php
+++ wp-content/themes/sentidos/header.php
@@ -22,7 +22,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-<link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/main.css" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/inc/css/main.css" />
 
 <script src="<?php echo get_template_directory_uri(); ?>/inc/js/modernizr.js" type="text/javascript"></script>
 
