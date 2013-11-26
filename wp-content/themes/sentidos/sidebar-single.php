3d33ef19338dfc95d5783c4cf0b992f112aac189
david s <messthetic@gmail.com>
2013-11-26 16:44:58 -0300

    ajustes sidebar single

--- wp-content/themes/sentidos/sidebar-single.php
+++ wp-content/themes/sentidos/sidebar-single.php
@@ -8,7 +8,7 @@
 
 		</article>
 		<article class="social">
-			<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
+			<div class="fb-like" data-href="<?php the_permalink(); ?>" data-width="40" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
 			<a href="https://twitter.com/share" class="twitter-share-button" data-lang="es">Twittear</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 		</article>
