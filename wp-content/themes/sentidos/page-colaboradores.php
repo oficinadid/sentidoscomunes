b5590d1d6bbaa95880feda5d6d64803727722c8c
david <david@OFICINADIDs-iMac.local>
2014-01-23 13:50:39 -0300

    sección colaboradores dinámica

--- wp-content/themes/sentidos/page-colaboradores.php
+++ wp-content/themes/sentidos/page-colaboradores.php
@@ -19,52 +19,96 @@
 					<section class="content">
 						<div id="the_content">
 
-								<?php while(the_repeater_field('colaboradores')): ?>
-	   									<div class="event">
-										<div class="img">
-											<?php if (get_sub_field('foto')): ?>
-												<?php echo wp_get_attachment_image( get_sub_field('foto'), '320x215' ); ?>
-											<?php endif ?>
-										</div>
-										<div class="desc">
-											<h3><?php the_sub_field('nombre')?></h3>
-											<p><?php the_sub_field('bio')?></p>
-											<ul class="meta">
-												<?php if (get_sub_field('twitter')): ?>
-													<li>
-														<strong>Twitter:</strong>
-														<span><?php the_sub_field('twitter')?></span>
-													</li>
-												<?php endif ?>
-												<?php if (get_sub_field('web')): ?>
-													<li>
-														<strong>Website:</strong>
-														<span><?php the_sub_field('web')?></span>
-													</li>
-												<?php endif ?>
-											</ul>
-										</div>
-										<div class="posts">
-											<h4>Publicaciones</h4>
-											<?php
-											$posts = get_sub_field('publicaciones');
-											 
-											if( $posts ): ?>
-												<ul>
-												<?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
-													<?php setup_postdata($post); ?>
-												    <li>
-												    	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-												    </li>
-												<?php endforeach; ?>
-												</ul>
-												<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-											<?php endif;?>
+							<?php
 
-	   									</div>
+								$args = array(
+
+									'role'      => 'subscriber',
+									'orderby'	=> 'name',
+									'order'		=> 'ASC'
+
+
+									);
+								$autores = get_users( $args );
+
+							?>
+
+
+							<?php foreach ($autores as $autor): ?>
+
+
+								<div class="event">
+									<div class="img">
+										<?php $avatar = get_field('avatar', 'user_' . $autor->ID); ?>
+
+										<?php if ($avatar): ?>
+											<img class="avatar" src="<?php echo $avatar ?>" alt="" width="100" height="100">
+
+										<?php else : ?>
+											<?php echo get_avatar($autor->ID); ?>
+										<?php endif ?>
 									</div>
+									<div class="desc">
+										<?php if ($autor->first_name): ?>
+											<h3><?php echo $autor->first_name ?> <?php echo $autor->last_name ?></h3>
+										<?php else: ?>
+											<h3><?php echo $autor->display_name ?></h3>
+										<?php endif ?>
 
-								<?php endwhile; ?>
+										<p><?php echo $autor->description ?></p>
+										<ul class="meta">
+
+											<?php if ($autor->twitter): ?>
+												<li>
+													<strong>Twitter:</strong>
+													<span>@<?php echo $autor->twitter ?></span>
+												</li>
+											<?php elseif($autor->user_url): ?>
+												<li>
+													<strong>Website:</strong>
+													<span><?php echo $autor->user_url ?></span>
+												</li>
+											<?php endif ?>
+
+										</ul>
+									</div>
+									<div class="posts">
+										<h4>Publicaciones</h4>
+
+										<?php $publicaciones = get_posts( array( 'author' => $autor->ID, 'posts_per_page' => 5, 'fields' => 'ids', 'no_found_rows' => true ) ); ?>
+
+										<?php $playlists = get_posts( array( 'post_type' => 'playlist', 'author' => $autor->ID, 'posts_per_page' => 5, 'fields' => 'ids', 'no_found_rows' => true ) ); ?>
+
+
+
+										<ul>
+
+										<?php $e=0; foreach ($playlists as $playlist): ?>
+											<?php $e++ ?>
+											<li>
+												<a href="<?php echo get_permalink($playlist); ?>"><?php echo get_the_title($playlist); ?></a>
+											</li>
+										<?php endforeach ?>
+
+										<?php $i=0; foreach ($publicaciones as $publicacion): ?>
+											<?php $i++ ?>
+
+											<li>
+												<a href="<?php echo get_permalink($publicacion); ?>"><?php echo get_the_title($publicacion); ?></a>
+											</li>
+
+
+											<?php if ($i == 5): ?>
+												<a href="<?php echo get_author_posts_url( $autor->ID ); ?>">Ver más posts</a>
+											<?php endif ?>
+										<?php endforeach ?>
+										</ul>
+
+
+   									</div>
+								</div>
+
+							<?php endforeach ?>
 
 						</div>
 					</section>
