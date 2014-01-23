8aded3791d98a20876beb6a412eea5394cbbff60
david <david@OFICINADIDs-iMac.local>
2014-01-23 13:49:11 -0300

    agregamos paginadores

--- wp-content/themes/sentidos/archive.php
+++ wp-content/themes/sentidos/archive.php
@@ -44,12 +44,20 @@
 			<?php while ( have_posts() ) : the_post(); ?>
 				<article>
 					<strong><?php $category = get_the_category(); echo $category[0]->cat_name; ?></strong>
+
+					<?php var_dump(get_the_author()) ?>
 					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 					<?php the_excerpt(); ?>
 					<a href="<?php the_permalink(); ?>" class="readmore">Seguir Leyendo</a>
 				</article>
 			 <?php endwhile; ?>
 		</div>
+
+		<?php 
+			if(function_exists('wp_pagenavi')) {
+			wp_pagenavi();
+			}
+		?>
 	</section>
 
 <?php get_footer(); ?>
--- wp-content/themes/sentidos/category.php
+++ wp-content/themes/sentidos/category.php
@@ -20,6 +20,7 @@
 		<div class="full">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<article>
+					
 					<strong><?php $category = get_the_category(); echo $category[0]->cat_name; ?></strong>
 					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 					<?php the_excerpt(); ?>
